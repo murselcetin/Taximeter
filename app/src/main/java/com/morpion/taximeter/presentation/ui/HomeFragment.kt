9e188af5943023634f6a50a2a0ca01806af7452e
Mürsel <murselcetin51@gmail.com>
2023-08-02 20:01:53 +0300

    Add home page design

---
+++ app/src/main/java/com/morpion/taximeter/presentation/ui/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.morpion.taximeter.presentation.ui
+
+import android.os.Bundle
+import android.view.View
+import com.morpion.taximeter.databinding.FragmentHomeBinding
+import com.morpion.taximeter.presentation.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
